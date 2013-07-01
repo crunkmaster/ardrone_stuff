8da167797ad1ffe902a394b61525eb7cbeef150b
viki <viki@ROS.(none)>
2013-07-01 12:55:15 -0400

    changed message type

--- src/listen_navdata.cpp
+++ src/listen_navdata.cpp
@@ -1,17 +1,20 @@
 /* some code to just print everything received from the navdata topic */
 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <ardrone_autonomy/Navdata.h>
 
-void chatterCallback( const std_msgs::String::ConstPtr &msg ) {
-	ROS_INFO("[%s]", msg -> data.c_str() ) ;
+void navdata_callback( const ardrone_autonomy::Navdata &msg ) {
+	int battery = msg.batteryPercent ;
+	cout << "battery percent: " << battery << endl ;
 }
 
 int main( int argc, char *argv[] ) {
-	
 	ros::init( argc, argv, "cool_navdata_node" ) ;
 	ros::NodeHandle nh ;
-	ros::Subscriber navdata_sub = nh.subscribe("ardrone/navdata", 1000, chatterCallback ) ;
+	ros::Subscriber navdata_sub ;
+
+	navdata_sub = nh.subscribe("/ardrone/navdata", 1, navdata_callback ) ;
 	ros::spin() ;
 
 	return 0 ;	
